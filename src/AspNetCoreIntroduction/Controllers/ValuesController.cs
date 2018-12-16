66f4553f7151555a584e2bfaa7f1591d571e5389
Alex Kamsteeg <alex@atlex.nl>
2018-12-16 09:27:57 +0100

    Basic project structure

---
+++ src/AspNetCoreIntroduction/Controllers/ValuesController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCoreIntroduction.Controllers
+{
+    [Route("api/values")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet("random")]
+        public ActionResult<int> Random()
+        {
+            var random = new Random();
+
+            var result = random.Next();
+
+            return result;
+        }
+    }
+}
